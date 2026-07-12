a276b5dd2ac333e5c58f02cd6a72173c96595dc4
Drakxard <drakxard@gmail.com>
2026-07-12 12:14:58 -0300

    Revert "fs"
    This reverts commit be06afe523cbae86d1f8529c26e6cfa412e393ac.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { SubjectWheel } from "@/components/subject-wheel"
 import { getRequestAuthSession } from "@/lib/authz"
-import { redirect } from "next/navigation"
 
 type HomePageProps = {
   searchParams: Promise<{
@@ -11,7 +10,7 @@ type HomePageProps = {
 export default async function Home({ searchParams }: HomePageProps) {
   const session = await getRequestAuthSession()
   if (!session) {
-    redirect("/login?next=/")
+    return null
   }
 
   return <SubjectWheel authSession={session} initialSearchParams={await searchParams} />
